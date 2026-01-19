dcb0ced5007fc3223b49876531575f2fd570cb81
yosbany <ytejas.86@gmail.com>
2026-01-19 18:15:55 -0300

    Agregar todas las herramientas: generador de iconos, servidor local y scripts de actualización

---
+++ tools/generate-icons/generate-icons.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+Generador de iconos para PWA
+Reemplaza los generadores HTML/JS anteriores
+"""
+
+if __name__ == "__main__":
+    print("Generador de iconos - En desarrollo")
